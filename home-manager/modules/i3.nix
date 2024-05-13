43ae477927af610df6b0d82da75a97a39c266f68
AdrisGithub <adrianjohn6543@gmail.com>
2024-05-13 19:46:42 +0200

    add customized i3

--- home-manager/modules/i3.nix
+++ home-manager/modules/i3.nix
@@ -3,5 +3,23 @@
 	home.file.".xinitrc".text = "exec i3";
 	xsession.windowManager.i3 = {
 		enable = true;
+		config = {
+			modifier = "Mod4";
+			terminal = "${pkgs.alacritty}/bin/alacritty";
+			menu = "${pkgs.dmenu}/bin/dmenu_run";
+			fonts = {
+				names = [ "JetBrainsMono Nerd Font" ];
+				style = "";
+				size = 8.0;
+			};
+			bars = [{
+				position = "bottom";
+				fonts = {
+					names = [ "JetBrainsMono Nerd Font"];
+					size = 7.5;
+				};
+				statusCommand = "${pkgs.i3status}/bin/i3status";
+			}];
+		};
 	};
 }
