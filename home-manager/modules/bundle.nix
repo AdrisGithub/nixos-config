f1bb4278fdd3a22f3a67dab1e8b24d42d9d273bf
AdrisGithub <adrianjohn6543@gmail.com>
2024-05-14 11:49:28 +0200

    add comments to the wm's

--- home-manager/modules/bundle.nix
+++ home-manager/modules/bundle.nix
@@ -4,7 +4,9 @@
                 ./zsh.nix
                 ./packages.nix
                 ./i3.nix
+		# needs years to compile
 		# ./herbstluftwm.nix
+		# cant get this to work rn
 		# ./xmonad.nix
 		./i3bar.nix
 		./flameshot.nix
