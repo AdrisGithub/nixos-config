53f3212707db135bf42fffdfabea2eeb994206f8
AdrisGithub <adrianjohn6543@gmail.com>
2024-05-30 12:59:50 +0200

    rm user pkgs

--- laptop/nixos/modules/users.nix
+++ laptop/nixos/modules/users.nix
@@ -6,6 +6,6 @@
     		isNormalUser = true;
     		description = "adri";
     		extraGroups = [ "networkmanager" "wheel" ];
-  		packages = with pkgs; [ prismlauncher-unwrapped ];
+  		packages = with pkgs; [ ];
 	};
 }
