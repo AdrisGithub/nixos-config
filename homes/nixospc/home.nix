b1d87b8314eb65c0a3e08111ad77d505439f103b
AdrisGithub <adrianjohn6543@gmail.com>
2024-06-11 21:38:58 +0200

    add zsh

--- homes/nixospc/home.nix
+++ homes/nixospc/home.nix
@@ -7,7 +7,8 @@
     stateVersion = "24.05";
   };
   imports = [
-  ];
+  	./modules
+	];
   nixpkgs.config.allowUnfree = true;
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
---
+++ homes/nixospc/modules/zsh.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, system, inputs, ... }: {
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    # enableAutosuggestions = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases =
+      let
+        flakeDir = "~/nix";
+      in
+      {
+        nvim = "${inputs.nvim-adri.packages.${system}.default}/bin/nvim";
+        nix-shell = "${pkgs.cached-nix-shell}/bin/cached-nix-shell";
+        nrs = "sudo nixos-rebuild switch --flake ${flakeDir}";
+        nfu = "sudo nix flake update ${flakeDir}";
+        "nix develop" = "nix develop -c $SHELL";
+      };
+
+    history.size = 10000;
+    history.path = "${config.xdg.dataHome}/zsh/history";
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "sudo" ];
+      theme = "robbyrussell";
+    };
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
+  };
+}
