12d5559edb3da3a9e6b372c0721495b88703f1f3
AdrisGithub <adrianjohn6543@gmail.com>
2024-07-18 11:33:16 +0200

    add mysql

--- hosts/bsl/configuration.nix
+++ hosts/bsl/configuration.nix
@@ -30,6 +30,12 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+  };
+
   services.logind.extraConfig = ''
           	# don’t shutdown when power button is short-pressed
             HandlePowerKey=ignore
